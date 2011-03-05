f775a2f293cdf1fd2b3211f18db8949a89853680
Darrell Furlong <darrell.furlong@gmail.com>
2011-03-05 10:26:20 -0500

    Removed default Rails Page Added _header and _footer and -stylesheet partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  #get "pages/home"
 
-  get "pages/contact"
+  #get "pages/contact"
 
-  get "pages/about"
+  #get "pages/about"
 
-  get "pages/help"
+  #get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,4 +63,9 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 end
